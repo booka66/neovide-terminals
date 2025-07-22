aa2e9f513ec4255cce41faa06bde427a9781470e
Jacob Cahoon <booka66@Jacobs-MacBook-Pro.local>
2025-07-21 23:31:00 -0600

    linting

--- plugin/smart-terminals.lua
+++ plugin/smart-terminals.lua
@@ -1,14 +1,15 @@
 if vim.g.loaded_smart_terminals then
-  return
+	return
 end
 vim.g.loaded_smart_terminals = true
 
 -- Auto-setup if not explicitly called
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if not vim.g.smart_terminals_setup_called then
-      vim.g.smart_terminals_setup_called = true
-      require("smart-terminals").setup()
-    end
-  end,
+	callback = function()
+		if not vim.g.smart_terminals_setup_called then
+			vim.g.smart_terminals_setup_called = true
+			require("smart-terminals").setup()
+		end
+	end,
 })
+
